236ad1ad89cf73604abf1462aa517168dbe0b03d
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-03-20 22:46:26 +0400

    Update components.py
    add navigation bar function component

--- enact/components.py
+++ enact/components.py
@@ -1,4 +1,4 @@
-import elements
+from . import elements
 
 class Component:
     def __init__(self):
@@ -23,23 +23,15 @@ class Card(Component):
             )
 
 
-# example use case
-new_card = Card(
-                'Altea', 
-                'https://i.pinimg.com/736x/4a/21/82/4a218232896756f33cbfe54b39aa45a0.jpg', 
-                description='Discover the charm of Altea, a picturesque town with stunning coastal views and colorful streets. Plan your visit to Altea and immerse yourself in its rich culture and breathtaking scenery.'
-                )
-print('\n',new_card)
-
-cards = elements.Flex(*(new_card for _ in range(20)))
-cards_html = '<html><head></head><body>' + str(cards) + '</body></html>'
-print('\n',cards_html)
-
-
-import os
-
-folder = os.path.dirname(__file__)
-with open(os.path.join(folder,'example.html'), 'w') as f:
-    f.write(cards_html)
-
-print('Saved in ', folder)
+def navbar(links: dict, logo_url: str = None):
+    lis = [elements.Anch(value, key) for key, value in links.items()]
+    ul = elements.Ul(*lis, arg_str='style="list-style-type: none; margin: 0; padding: 0;"')
+    if logo_url is None:
+        node = elements.Nav(ul, arg_str='class="navbar"')
+    else:
+        node = elements.Nav(elements.Img(
+            f'src={logo_url} height="50px" alt="logo"'), 
+            ul, 
+            arg_str='class="navbar"'
+            )
+    return str(node)
